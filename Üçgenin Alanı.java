f42271b6234ddbe94edaa82a63e9d2e44d942903
kaan64-ops <kaancaliskan1971@gmail.com>
2026-02-05 16:10:45 +0300

    Create Üçgenin Alanı.java

---
+++ Üçgenin Alanı.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Main{
+    static void main(String[] args) {
+        Scanner imp = new Scanner(System.in);
+        double a,b,c;
+        System.out.println("Üçgenin kenar uzunluklarını giriniz : ");
+        a=imp.nextDouble();
+        b=imp.nextDouble();
+        c=imp.nextDouble();
+        double u =(a+b+c)/2;
+        double alannınKaresi = u*(u-a)*(u-b)*(u-c);
+        double alan= Math.sqrt(alannınKaresi);
+        System.out.print("üçgenin alanı : " + alan);
+
+
+    }
+}
