8a5f40e7cdd82e8bf5a4f0c73fd3d4d53f867faa
kaan64-ops <kaancaliskan1971@gmail.com>
2026-02-05 16:23:15 +0300

    Create Taksimetre Programı.java

---
+++ Taksimetre Programı.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Main{
+    static void main(String[] args) {
+        Scanner imp = new Scanner(System.in);
+        double km;
+        System.out.print("Kaç kilometre gittiğini yazınız : ");
+        km=imp.nextDouble();
+        double tutar = km*2.20+10;
+        tutar = (tutar<=20) ? 20:tutar;
+        System.out.println("Ödenecek tutar : "+tutar );
+
+
+    }
+}
