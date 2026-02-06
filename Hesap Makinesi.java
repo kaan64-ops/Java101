ec9560e8b553aa9294e47875cbf23b7ff782201f
kaan64-ops <kaancaliskan1971@gmail.com>
2026-02-06 13:16:06 +0300

    Create  Hesap Makinesi.java

---
+++ Hesap Makinesi.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+public class Main {
+    static void main(String[] args) {
+        Scanner imp = new Scanner(System.in);
+        int n1,n2,select;
+        System.out.print("İlk sayıyı giriniz : ");
+        n1= imp.nextInt();
+        System.out.print("İkinci sayıyı giriniz : ");
+        n2= imp.nextInt();
+        System.out.println("1-Toplama\n2-Çıkarma\n3-Çarpma\n4-Bölme");
+        System.out.println("Seçiniz : ");
+        select=imp.nextInt();
+        switch (select){
+            case(1):
+                System.out.println("Toplam : "+ (n1+n2));
+                break;
+            case(2):
+                System.out.println("Çıkarma : "+ (n1-n2));
+                break;
+            case(3):
+                System.out.println("Çarpma : "+(n1*n2));
+                break;
+            case(4):
+                System.out.println("Bölme : "+(n1/n2));
+                break;
+            default:
+                System.out.println("Yalnış seçim yaptınız. Tekrar deneyiniz.");
+
+        }
+
+    }
+}
