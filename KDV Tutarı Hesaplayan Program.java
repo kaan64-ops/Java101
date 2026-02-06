c26b84b5edd83538f4017eb67b644613fab5f154
kaan64-ops <kaancaliskan1971@gmail.com>
2026-02-06 13:54:15 +0300

    Update KDV Tutarı Hesaplayan Program.java

--- KDV Tutarı Hesaplayan Program.java
+++ KDV Tutarı Hesaplayan Program.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 public class Main{
-    static void main(String[] args) {
+   public static void main(String[] args) {
         double deger;
         Scanner imp = new Scanner(System.in);
         System.out.print("tutarı giriniz : " );
