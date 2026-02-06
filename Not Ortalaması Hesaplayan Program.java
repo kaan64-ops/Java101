0ce96cac94975478587fa0731b0fef730d131ee8
kaan64-ops <kaancaliskan1971@gmail.com>
2026-02-06 13:54:47 +0300

    Update Not Ortalaması Hesaplayan Program.java

--- Not Ortalaması Hesaplayan Program.java
+++ Not Ortalaması Hesaplayan Program.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 public class Main{
-    static void main(String[] args) {
+   public static void main(String[] args) {
         int mat,fiz,kim,tur,tarih,muz;
         Scanner imp = new Scanner(System.in);
         
